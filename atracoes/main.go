66dbe941d1902948811aeaf300c9cee2595d6776
Arthur Schurhaus <arthurschur@icloud.com>
2025-10-30 23:53:15 -0300

    added create atracao route

--- atracoes/main.go
+++ atracoes/main.go
@@ -4,17 +4,22 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	_ "modernc.org/sqlite"
 )
 
 type Atracao struct {
-	ID               string `json:"id"`
+	ID string `json:"id"`
+	CreateAtracaoRequest
+	CreatedAt string `json:"created_at"`
+}
+
+type CreateAtracaoRequest struct {
 	Nome             string `json:"nome"`
 	Capacidade       int    `json:"capacidade"`
 	TempoMedioEspera int    `json:"tempo_medio_espera"`
-	CreatedAt        string `json:"created_at"`
 }
 
 func main() {
@@ -27,11 +32,11 @@ func main() {
 	}
 	createTable := `
 	CREATE TABLE IF NOT EXISTS atracoes (
-		id TEXT PRIMARY KEY,
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		nome TEXT NOT NULL,
 		capacidade INTEGER NOT NULL,
 		tempoMedioEspera INTEGER NOT NULL,
-		created_at TEXT NOT NULL
+		created_at TEXT NOT NULL DEFAULT (datetime('now'))
 	);
 	`
 	if _, err := db.Exec(createTable); err != nil {
@@ -46,6 +51,7 @@ func main() {
 		w.Write([]byte("ok"))
 	})
 	mux.HandleFunc("GET /atracoes", handler.GetAllAtracoes)
+	mux.HandleFunc("POST /atracoes", handler.CreateAtracao)
 
 	fmt.Println("Atracoes service running on :8081")
 	http.ListenAndServe(":8081", mux)
@@ -76,5 +82,33 @@ func (h *AtracaoHandler) GetAllAtracoes(w http.ResponseWriter, r *http.Request) 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(atracoes); err != nil {
 		http.Error(w, "failed to encode attractions", http.StatusInternalServerError)
+	}
+}
+
+func (h *AtracaoHandler) CreateAtracao(w http.ResponseWriter, r *http.Request) {
+	var newAtracao CreateAtracaoRequest
+	err := json.NewDecoder(r.Body).Decode(&newAtracao)
+	if err != nil {
+		http.Error(w, "Error decoding attraction", http.StatusBadRequest)
+		return
+	}
+
+	if newAtracao.Nome == "" || newAtracao.Capacidade <= 0 || newAtracao.TempoMedioEspera <= 0 {
+		http.Error(w, "nome, capacidade and tempoMedioEspera are required fields", http.StatusBadRequest)
+		return
+	}
+
+	insertSQL := "INSERT INTO atracoes (nome, capacidade, tempoMedioEspera) VALUES (?, ?, ?)"
+	_, err = h.db.Exec(insertSQL, newAtracao.Nome, newAtracao.Capacidade, newAtracao.TempoMedioEspera)
+	if err != nil {
+		slog.Error("Error inserting attraction into db", "error", err)
+		http.Error(w, "Error inserting attraction into db", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(newAtracao); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 	}
 }
